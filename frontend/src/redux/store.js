d033f54bb5b7d5d329400a2344c345c1f9fdf432
wamiquem <wamique.ansari@sjsu.edu>
2019-11-07 21:59:07 -0800

    Added fix for redux  devtools extension handling in case there is no extension in the browser

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,17 +3,39 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 // import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware)
+//    ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
 const initialState = {};
 
 const middleware = [thunk];
 
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware)
-   ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  enhancer
 );
 
 export default store;
